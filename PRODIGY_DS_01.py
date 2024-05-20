4a68ad271deb3a1a74a4aedb9a2f2f984f03733a
roshrr10 <71438492+roshrr10@users.noreply.github.com>
2024-05-20 13:00:50 +0530

    Update PRODIGY_DS_01.py

--- PRODIGY_DS_01.py
+++ PRODIGY_DS_01.py
@@ -7,7 +7,7 @@
 # 
 # AIM: To analyze the distribution of various features in the insurance dataset.
 # 
-# The dataset contains information about individuals, including their age, gender, body mass index (BMI), and other attributes. We will create visualizations to understand the distributions of these features better.
+# The dataset contains information about individuals, including their age, gender, body mass index (BMI), and other attributes. I've created visualizations to understand the distributions of these features better.
 
 # In[1]:
 
